9aca2499b0c474f16f6cd7c73f7b4921d8f50eca
Jordan Greenberg <j@j13g.com>
2023-05-15 19:13:02 -0400

    adjust how waiting for signals works to make it a bit harder to do wrong

--- sig/sig.go
+++ sig/sig.go
@@ -5,12 +5,12 @@ import (
 	"os/signal"
 )
 
-func WaitForSignals(sigs ...os.Signal) {
+func WaitForSignals(sigs []os.Signal) {
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, sigs...)
 	<-sigChan
 }
 
 func WaitUntilSignalled() {
-	WaitForSignals(os.Kill, os.Interrupt)
+	WaitForSignals([]os.Signal{os.Kill, os.Interrupt})
 }
